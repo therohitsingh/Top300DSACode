e96b2ff833677d73ad197afcb39146969010315a
Rohit Raj Singh <therohitsinghr@gmail.com>
2021-10-02 23:48:17 +0530

    commit

---
+++ Basic Programming/demo.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = []
+for i in range(n):
+    a.append(int(input()))
+k = input())
+a.sort()
+for i in reversed(k):
+    print(a[i])
---
+++ Basic Programming/removeduplicateele.py
@@ -0,0 +1,5 @@
+from typing import Set
+
+
+a  = [1, 2, 2]
+print(set(a))
